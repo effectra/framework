fceb82f81410e47d6609b641b9788fafeb9236d8
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-08-17 18:38:16 +0200

    add new command Generate App Key

---
+++ src/Cmd/Key/GenerateKey.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Effectra\Core\Cmd\Key;
+
+use Effectra\Core\Application;
+use Effectra\Core\Console\ConsoleBlock;
+use Effectra\Core\Facades\Token;
+use Effectra\Core\Utils\EnvManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateKey extends Command
+{
+
+    protected function configure()
+    {
+        $this->setName('key:generate')
+            ->setDescription('Generate new Key');
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $command = implode(' ', array_slice($_SERVER['argv'], 1));
+        dd($command);
+
+        $io = new ConsoleBlock($input, $output);
+
+        $io->info('Generate Key');
+
+        $key = Token::generateToken(30);
+
+        $file = Application::appPath('.env');
+
+        $env = new EnvManager($file);
+
+        $env->set('APP_KEY', $key);
+
+        if ($env->save()) {
+            echo "  KEY:  $key\n";
+        }else{
+            echo "  Key generate failed\n";
+        }
+
+        return 0;
+    }
+}
