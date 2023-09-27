c81af98e267c839fb4ab317282f26afde0bac63d
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-09-27 23:12:38 +0200

    update modeling sturcture, add collection method

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -10,10 +10,16 @@ use Effectra\Database\Data\DataRules;
  * Class Model
  *
  * This class represents a base model for database operations.
- *
  */
 class Model
 {
+    protected static string $table = "";
+    /**
+     * @param array $data represent model data
+     */
+    public function __construct(public array $data = [])
+    {
+    }
     /**
      * Get a new instance of DataRules.
      *
@@ -44,5 +50,23 @@ class Model
     public static function display()
     {
         return static::rule()->integer('id');
+    }
+
+    /**
+     * get table name of model
+     */
+    public static function getTableName(): string
+    {
+        return static::$table;
+    }
+
+    /**
+     * Create a ModelCollection from the current instance's data.
+     *
+     * @return ModelCollection A new ModelCollection containing the current instance's data.
+     */
+    public function collection(): ModelCollection
+    {
+        return new ModelCollection($this->data);
     }
 }
--- src/Database/ModelBase.php
+++ src/Database/ModelBase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Effectra\Core\Database;
 
 use Effectra\Core\Application;
+use Effectra\Database\Data\DataOptimizer;
 use Effectra\Database\Data\DataRules;
 use Effectra\Database\DB;
 use Effectra\SqlQuery\Condition;
@@ -15,8 +16,6 @@ use Effectra\SqlQuery\Query;
  */
 trait ModelBase
 {
-    protected static array $data = [];
-
     /**
      * Get the database instance.
      *
@@ -30,11 +29,11 @@ trait ModelBase
     /**
      * Display data fetched from the database based on the model's data rules.
      *
-     * @param \PDOStatement $stmt The PDO statement containing the fetched data.
+     * @param DB $stmt The PDO statement containing the fetched data.
      *
-     * @return array|null An array of fetched data or null if no data rules are defined.
+     * @return ?array An array of fetched data or null if no data rules are defined.
      */
-    public static function displayData($stmt): array
+    public static function displayData($stmt): ?array
     {
         // Check if the method getDataRules() exists in the current class.
         if (!method_exists(static::class, 'getDataRules')) {
@@ -45,26 +44,18 @@ trait ModelBase
         $dataRules = static::getDataRules();
 
         if ($dataRules) {
+
             // Fetch data using the DataRules attributes and rules.
-            return $stmt->fetchPretty(function (DataRules $r) use ($dataRules) {
+            $data =  $stmt->fetchPretty(function (DataRules $r) use ($dataRules) {
                 $r->setAttributes($dataRules->getAttributes());
                 $r->setRules($dataRules->getRules());
             });
+        } else {
+            // If no data rules are defined, fetch data without formatting.
+            $data = $stmt->fetch();
         }
 
-        // If no data rules are defined, fetch data without formatting.
-        return $stmt->fetch();
-    }
-
-    /**
-     * Format the data in a pretty format.
-     * @param mixed $data The data to be inserted.
-     *
-     * @throws DataValidatorException If the data is not valid.
-     */
-    public static function optimizeData($data, callable $rules)
-    {
-        static::db()->prettyData($data, $rules);
+        return $data ? static::data($data)->data : null;
     }
 
     /**
@@ -74,18 +65,28 @@ trait ModelBase
      */
     public static function data($data)
     {
-        static::$data = $data;
-        return new static;
+        return new static($data);
+    }
+
+    /**
+     * Format the data in a pretty format.
+     * @param mixed $data The data to be inserted.
+     * @param callable $rules — A callback function to define rules using DataRules.
+     */
+    public static function dataOptimized($data, callable $rules)
+    {
+        $op = new DataOptimizer($data);
+        return static::data($op->optimize($rules));
     }
 
     /**
      * Retrieve all records from the model's table.
      *
-     * @return array|null The retrieved records, or null if no records found.
+     * @return ?array The retrieved records, or null if no records found.
      */
-    public static function all()
+    public static function all(): ?array
     {
-        $query = (string) Query::select(static::$table)->all();
+        $query = (string) Query::select(static::getTableName())->all();
         $stmt = static::db()->query($query);
 
         return static::displayData($stmt);
@@ -96,14 +97,15 @@ trait ModelBase
      *
      * @param mixed $id The ID of the record.
      * @param bool|null $associative Whether to return an associative array or object.
-     * @return object|null|array The retrieved record, or null if no record found.
+     * @return ?array The retrieved record, or null if no record found.
      */
-    public static function find($id, ?bool $associative = null)
+    public static function find($id, ?bool $associative = null): ?array
     {
-        $query = (string) Query::select(static::$table)->whereId($id)->limit(1);
+        $query = (string) Query::select(static::getTableName())->all()->where(['id' => $id])->limit(1);
         $data = static::db()->query($query);
         if ($data) {
-            return $associative ? static::displayData($data) : (object) static::displayData($data);
+            $data =  static::displayData($data);
+            return $associative ? $data : (object) $data;
         }
         return null;
     }
@@ -113,11 +115,11 @@ trait ModelBase
      *
      * @param Condition $conditions The conditions to match.
      * @param bool|null $associative Whether to return an associative array or object.
-     * @return object|null|array The retrieved record, or null if no record found.
+     * @return ?array The retrieved record, or null if no record found.
      */
-    public static function where(Condition $conditions, ?bool $associative = null)
+    public static function where(Condition $conditions, ?bool $associative = null): ?array
     {
-        $query = (string) Query::select(static::$table)->whereConditions($conditions);
+        $query = (string) Query::select(static::getTableName())->whereConditions($conditions);
         $data = static::db()->query($query)->fetch();
         if ($data) {
             return $associative ? static::displayData($data) : (object) static::displayData($data);
@@ -126,23 +128,45 @@ trait ModelBase
     }
 
     /**
+     * limit rows of table records.
+     * @param int $from
+     * @param null|int $to
+     * @return ?array The retrieved records, or null if no records found.
+     */
+    public static function limit(int $from, ?int $to = null): ?array
+    {
+        $query = Query::select(static::getTableName())->limit($from, $to);
+        $data = static::db()->query($query);
+
+        return static::displayData($data);
+    }
+
+    /**
+     * Search for records based on conditions.
+     *
+     * @param array $conditions The conditions to match.
+     * @return  ?array  The retrieved records, or null if no records found.
+     * @throws InvalidArgumentException If invalid conditions provided.
+     */
+    public static function search($conditions): ?array
+    {
+        if (empty($conditions)) {
+            throw new \InvalidArgumentException('Invalid conditions.');
+        }
+        $query = (string) Query::select(static::getTableName())->columns(array_keys($conditions))->whereLike($conditions);
+        $data = static::db()->query($query);
+
+        return static::displayData($data);
+    }
+
+    /**
      * Create a new record. 
      *
      * @return bool — True if the data was successfully inserted, false otherwise.
      */
-    public static function create()
+    public function create()
     {
-        return static::db()->table(static::$table)->insert(static::$data);
-    }
-
-    /**
-     * Get the ID of the last inserted row.
-     * 
-     * @return string|false — Returns the last inserted ID or false on failure.
-     */
-    public static function getId(): string|false
-    {
-        return static::db()->lastInsertId();
+        return static::db()->table(static::getTableName())->insert($this->data);
     }
 
     /**
@@ -151,9 +175,9 @@ trait ModelBase
      * @param mixed $id The ID of the record to update.
      * @return bool Whether the update operation was successful.
      */
-    public static function update($id)
+    public function update($id)
     {
-        return static::db()->table(static::$table)->update(static::$data, (new Condition())->where(['id' => $id]));
+        return static::db()->table(static::getTableName())->update($this->data, (new Condition())->where(['id' => $id]));
     }
 
     /**
@@ -162,27 +186,9 @@ trait ModelBase
      * @param Condition $conditions The conditions of the record to update.
      * @return bool Whether the update operation was successful.
      */
-    public static function updateWhere(Condition $conditions)
+    public function updateWhere(Condition $conditions)
     {
-        return static::db()->table(static::$table)->update(static::$data, $conditions);
-    }
-
-    /**
-     * Search for records based on conditions.
-     *
-     * @param array $conditions The conditions to match.
-     * @return array|null The retrieved records, or null if no records found.
-     * @throws InvalidArgumentException If invalid conditions provided.
-     */
-    public static function search($conditions)
-    {
-        if (empty($conditions)) {
-            throw new \InvalidArgumentException('Invalid conditions.');
-        }
-        $query = (string) Query::select(static::$table)->whereLike($conditions);
-        $data = static::db()->query($query);
-
-        return static::displayData($data);
+        return static::db()->table(static::getTableName())->update($this->data, $conditions);
     }
 
     /**
@@ -193,7 +199,18 @@ trait ModelBase
      */
     public static function delete($id)
     {
-        return static::db()->table(static::$table)->delete((new Condition())->where(['id' => ':id']), ['id' => $id]);
+        return static::db()->table(static::getTableName())->delete((new Condition())->where(['id' => ':id']), ['id' => $id]);
+    }
+
+    /**
+     * Delete a record by ID.
+     *
+     * @param Condition $conditions The conditions of the record to update.
+     * @return bool Whether the delete operation was successful.
+     */
+    public static function deleteWhere(Condition $conditions)
+    {
+        return static::db()->table(static::getTableName())->delete((new Condition())->whereConditions($conditions));
     }
 
     /**
@@ -213,21 +230,18 @@ trait ModelBase
      */
     public static function truncate()
     {
-        $query = Query::truncate(static::$table);
+        $query = Query::truncate(static::getTableName());
         return static::db()->query($query)->run();
     }
 
     /**
-     * limit rows of table records.
-     * @param int $from
-     * @param null|int $to
-     * @return array|null The retrieved records, or null if no records found.
+     * Get the ID of the last inserted row.
+     * 
+     * @return string|false — Returns the last inserted ID or false on failure.
      */
-    public static function limit(int $from, ?int $to = null): ?array
+    public static function getId(): string|false
     {
-        $query = Query::select(static::$table)->limit($from, $to);
-        $data = static::db()->query($query);
-
-        return static::displayData($data);
+        return static::db()->lastInsertId();
     }
+
 }
