e7d46377c7f64452ceffd95f765e8f3d2b69e0bf
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-11-24 16:48:37 +0100

    update json method to handle data from Model class

--- src/Response.php
+++ src/Response.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Effectra\Core;
 
+use Effectra\Database\Model;
+use Effectra\DataOptimizer\Contracts\DataCollectionInterface;
 use Effectra\Http\Extensions\ResponseExtension;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class Response
@@ -14,4 +17,20 @@ use Effectra\Http\Extensions\ResponseExtension;
 class Response extends ResponseExtension
 {
 
+    public function json($data, int $status_code = 200, array $headers = []): ResponseInterface
+    {
+        if ($data instanceof Model) {
+            $data = $data->toArray();
+        }
+
+        if ($data instanceof DataCollectionInterface) {
+            $new = [];
+            foreach ($data as $item) {
+                $new[] = $item->toArray();
+            }
+            $data = $new;
+        }
+
+        return parent::json($data, $status_code, $headers);
+    }
 }
