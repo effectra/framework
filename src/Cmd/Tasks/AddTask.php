814f3d40511473f59dc059c560f8f52f6bf8a805
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2024-01-02 10:03:45 -0800

    add  Tasks services to  console app

---
+++ src/Cmd/Tasks/AddTask.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Effectra\Core\Cmd\Tasks;
+
+use Effectra\Core\Application;
+use Effectra\Core\Console\ConsoleBlock;
+use Effectra\Core\Log\ConsoleLogTrait;
+use Effectra\Core\Tasks\CronScheduler;
+use Effectra\Fs\Path;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class AddTask extends Command
+{
+    use ConsoleLogTrait;
+
+    protected function configure()
+    {
+        $this->setName('task:add')
+            ->setDescription('Add task to cron job')
+            ->addArgument('name', InputArgument::REQUIRED, 'The script filename')
+            ->addOption('m', null, InputOption::VALUE_OPTIONAL, 'add to cron command', '*')
+            ->addOption('h', null, InputOption::VALUE_OPTIONAL, 'add to cron command', '*')
+            ->addOption('d', null, InputOption::VALUE_OPTIONAL, 'add to cron command', '*')
+            ->addOption('mth', null, InputOption::VALUE_OPTIONAL, 'add to cron command', '*')
+            ->addOption('w', null, InputOption::VALUE_OPTIONAL, 'add to cron command', '*');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->log($this->getName(), __FILE__);
+
+        $io = new ConsoleBlock($input, $output);
+
+        $io->info('Generate Provider File');
+
+        $cron = new CronScheduler();
+
+        $path = Application::appPath('scripts' . Path::ds() . $input->getArgument('name'));
+
+        $cron->addJob(
+            $input->getOption('m'),
+            $input->getOption('h'),
+            $input->getOption('d'),
+            $input->getOption('mth'),
+            $input->getOption('w'),
+            $path,
+        );
+
+        $cron->save();
+
+        return 0;
+    }
+}
