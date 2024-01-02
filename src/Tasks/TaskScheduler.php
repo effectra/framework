05ba05e940ff60a42a5185f1f8d5e84925c8bce2
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2024-01-02 09:42:22 -0800

    convert var tasks and methods of class to static

--- src/Tasks/TaskScheduler.php
+++ src/Tasks/TaskScheduler.php
@@ -9,31 +9,32 @@ namespace Effectra\Core\Tasks;
  */
 class TaskScheduler
 {
-    protected array $tasks = [];
+    protected static array $tasks = [];
 
     /**
      * Schedule a task to be executed at a specific time.
      *
-     * @param string $time     The time at which the task should be executed.
-     * @param mixed  $callback The callback function or method to be executed.
+     * @param string   $time     The time at which the task should be executed.
+     * @param \Closure $callback The callback function or method to be executed.
+     * @param array    $args     The arguments for the callback function or method.
      */
-    public function schedule(string $time, $callback): void
+    public static function schedule(string $time, \Closure $callback, array $args = []): void
     {
-        $this->tasks[] = compact('time', 'callback');
+        static::$tasks[] = compact('time', 'callback', 'args');
     }
 
     /**
      * Run the scheduled tasks.
      */
-    public function run(): void
+    public static function run(): void
     {
         $now = time();
 
-        foreach ($this->tasks as $task) {
+        foreach (static::$tasks as $task) {
             $taskTime = strtotime($task['time']);
 
             if ($taskTime <= $now) {
-                call_user_func($task['callback']);
+                call_user_func($task['callback'], $task['args']);
             }
         }
     }
