71f2af0a9c1c0eb39e0a507e2d17702402ff335a
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2024-01-03 12:31:47 -0800

    add docBlocks

--- src/Authentication/AppAuth.php
+++ src/Authentication/AppAuth.php
@@ -7,6 +7,11 @@ namespace Effectra\Core\Authentication;
 use Effectra\Config\ConfigFile;
 use Effectra\Core\Application;
 
+/**
+ * Class AppAuth
+ *
+ * Helper class for authentication-related configuration in the application.
+ */
 class AppAuth
 {
     /**
@@ -16,7 +21,10 @@ class AppAuth
      */
     private static function getConfig()
     {
+        // Path to the auth configuration file
         $file = Application::configPath('auth.php');
+
+        // Read the configuration from the file
         $configFile = new ConfigFile($file);
         $config = $configFile->read();
 
