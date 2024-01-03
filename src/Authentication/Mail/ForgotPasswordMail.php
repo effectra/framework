3e1c7b54b8b0bcae38d93ea838b4868f118bb2be
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2024-01-03 12:33:56 -0800

    add docBlocks

--- src/Authentication/Mail/ForgotPasswordMail.php
+++ src/Authentication/Mail/ForgotPasswordMail.php
@@ -6,11 +6,24 @@ namespace Effectra\Core\Authentication\Mail;
 
 use Effectra\Core\Mail\Mail;
 
+/**
+ * Class ForgotPasswordMail
+ *
+ * Mail class for sending a forgot password email.
+ *
+ * @package Effectra\Core\Authentication\Mail
+ */
 class ForgotPasswordMail extends Mail
 {
+    /**
+     * ForgotPasswordMail constructor.
+     *
+     * @param string $email The email address to send the mail to.
+     * @param string $link  The password reset link.
+     */
     public function __construct(
-        private string  $email,
-        private string  $link,
+        private string $email,
+        private string $link
     ) {
         parent::__construct();
         $this
@@ -20,11 +33,21 @@ class ForgotPasswordMail extends Mail
             ->text($this->templateText());
     }
 
+    /**
+     * Generate the text template for the email.
+     *
+     * @return string
+     */
     public function templateText()
     {
-        return sprintf('click to this link to verify your account: %s', $this->link);
+        return sprintf('Click on this link to verify your account: %s', $this->link);
     }
 
+    /**
+     * Generate the HTML template for the email.
+     *
+     * @return string
+     */
     public function templateHtml()
     {
         return sprintf('<p>%s</p>', $this->templateText());
--- src/Authentication/Mail/VerifyUserMail.php
+++ src/Authentication/Mail/VerifyUserMail.php
@@ -6,11 +6,24 @@ namespace Effectra\Core\Authentication\Mail;
 
 use Effectra\Core\Mail\Mail;
 
+/**
+ * Class VerifyUserMail
+ *
+ * Mail class for sending a user verification email.
+ *
+ * @package Effectra\Core\Authentication\Mail
+ */
 class VerifyUserMail extends Mail
 {
+    /**
+     * VerifyUserMail constructor.
+     *
+     * @param string $email The email address to send the mail to.
+     * @param string $link  The verification link.
+     */
     public function __construct(
-        private string  $email,
-        private string  $link,
+        private string $email,
+        private string $link
     ) {
         parent::__construct();
         $this
@@ -20,13 +33,23 @@ class VerifyUserMail extends Mail
             ->text($this->templateText());
     }
 
+    /**
+     * Generate the text template for the email.
+     *
+     * @return string
+     */
     public function templateText()
     {
-        return sprintf('click to this link to verify your account: %s', $this->link);
+        return sprintf('Click on this link to verify your account: %s', $this->link);
     }
 
+    /**
+     * Generate the HTML template for the email.
+     *
+     * @return string
+     */
     public function templateHtml()
     {
-        return sprintf('<p style="margin: 20px,text-weight: 400">%s</p>', $this->templateText());
+        return sprintf('<p style="margin: 20px; text-weight: 400;">%s</p>', $this->templateText());
     }
 }
