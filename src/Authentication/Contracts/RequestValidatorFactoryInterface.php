c3dd75bb7d0b7e8f0df89f7cca74dee0a8c3258d
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2024-01-03 12:32:08 -0800

    add docBlocks

--- src/Authentication/Contracts/RequestValidatorFactoryInterface.php
+++ src/Authentication/Contracts/RequestValidatorFactoryInterface.php
@@ -1,10 +1,24 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Effectra\Core\Authentication\Contracts;
 
+/**
+ * Interface RequestValidatorFactoryInterface
+ *
+ * Defines the contract for creating instances of RequestValidatorInterface.
+ *
+ * @package Effectra\Core\Authentication\Contracts
+ */
 interface RequestValidatorFactoryInterface
 {
+    /**
+     * Create an instance of RequestValidatorInterface based on the provided class name.
+     *
+     * @param string $class
+     *
+     * @return RequestValidatorInterface
+     */
     public function make(string $class): RequestValidatorInterface;
 }
--- src/Authentication/Contracts/RequestValidatorInterface.php
+++ src/Authentication/Contracts/RequestValidatorInterface.php
@@ -1,10 +1,24 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Effectra\Core\Authentication\Contracts;
 
+/**
+ * Interface RequestValidatorInterface
+ *
+ * Defines the contract for request data validation operations.
+ *
+ * @package Effectra\Core\Authentication\Contracts
+ */
 interface RequestValidatorInterface
 {
+    /**
+     * Validate the provided data and return the validated data.
+     *
+     * @param array $data
+     *
+     * @return array
+     */
     public function validate(array $data): array;
 }
