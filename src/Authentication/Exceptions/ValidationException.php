4cb7bdb7ffbfbdab6a8b2625a34550c8e828211e
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2024-01-03 12:33:39 -0800

    add docBlocks

--- src/Authentication/Exceptions/ValidationException.php
+++ src/Authentication/Exceptions/ValidationException.php
@@ -6,9 +6,25 @@ namespace Effectra\Core\Authentication\Exceptions;
 
 use Throwable;
 
+/**
+ * Class ValidationException
+ *
+ * Exception thrown when there are validation errors.
+ *
+ * @package Effectra\Core\Authentication\Exceptions
+ */
 class ValidationException extends \RuntimeException
 {
-    public function __construct(public array $errors,string $message = 'Validation Error(s)',int $code = 422,?Throwable $previous = null) {
-        parent::__construct($message,$code,$previous);
+    /**
+     * ValidationException constructor.
+     *
+     * @param array         $errors   The validation errors.
+     * @param string        $message  The exception message.
+     * @param int           $code     The exception code.
+     * @param Throwable|null $previous The previous throwable used for the exception chaining.
+     */
+    public function __construct(public array $errors, string $message = 'Validation Error(s)', int $code = 422, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
     }
 }
